7292c082fc1a8880f8df4a88a6eed4034429ccf3
Fabio  Araujo <wmf.araujo@gmail.com>
2024-09-20 23:21:45 -0300

    Add Components

---
+++ client/components/Login.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Call API to login
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Email:</label>
+      <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <br />
+      <label>Password:</label>
+      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <br />
+      <button type="submit">Login</button>
+    </form>
+  );
+}
+
+export default Login;
---
+++ client/components/Register.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+function Register() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Call API to register
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Name:</label>
+      <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+      <br />
+      <label>Email:</label>
+      <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <br />
+      <label>Password:</label>
+      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <br />
+      <button type="submit">Register</button>
+    </form>
+  );
+}
+
+export default Register;
